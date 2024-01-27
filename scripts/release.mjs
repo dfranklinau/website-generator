a03b85d0e911a86790e0eacbc6a7ce5f3f435f45
Daniel Franklin <git@danielfranklin.id.au>
2024-01-27 23:05:58 +1000

    Create a `release` script to generate a package archive of `packages/website-generator`
    The purpose of the package archive is to allow the subdirectory,
    `packages/website-generator` to be installable. This will mean creating
    a release in GitHub, generating the package archive and attaching it to
    the release so that it can be referenced in the `package.json` of any
    other project.

---
+++ scripts/release.mjs
@@ -0,0 +1,43 @@
+import fs from "fs-extra";
+import path from "path";
+import tar from "tar";
+
+// The version number to use in file names and directories.
+const NPM_PACKAGE_VERSION = process.env.npm_package_version
+
+// A temporary directory to prepare the release archive.
+const TMP_RELEASE = ".tmp/release";
+
+// The root directory of the release.
+const ROOT = `website-generator-${NPM_PACKAGE_VERSION}`;
+
+// The files and directories to copy to the release archive.
+const SOURCE = [
+  "LICENSE",
+  "packages/website-generator/README.md",
+  "packages/website-generator/bin",
+  "packages/website-generator/dist",
+  "packages/website-generator/package.json",
+];
+
+// Set up the temporary directory to prepare the release archive.
+await fs.mkdirp(TMP_RELEASE);
+await fs.emptydir(TMP_RELEASE);
+
+// Copy all required files and directories to the release archive.
+await Promise.all(SOURCE.map(async (item) => {
+  const basename = path.basename(item);
+  return fs.copy(item, path.join(TMP_RELEASE, ROOT, basename));
+}));
+
+// Create an archive from the array of source files, using the version number
+// from `package.json` in the file name. Set the `cwd` so that the root
+// directory is the value of `ROOT` and does not include `TMP_RELEASE`.
+tar.c(
+  {
+    cwd: path.join(TMP_RELEASE),
+    file: `website-generator-${NPM_PACKAGE_VERSION}.tar.gz`,
+    gzip: true,
+  },
+  [ROOT]
+);
